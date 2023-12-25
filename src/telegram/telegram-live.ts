252abe89cda59fdfd24e739891eb386f20aa6587
hulk19 <phamhungbkhn@gmail.com>
2023-12-25 16:46:24 +0700

    wip: redis

--- src/telegram/telegram-live.ts
+++ src/telegram/telegram-live.ts
@@ -1,11 +1,18 @@
 import TelegramSession from "./telegram-session";
 import {NewMessage} from "telegram/events";
+import {FUND_NOTIFICATION} from "../utils/constants";
+import Redis from 'ioredis'
+import {Api} from "telegram";
 /*
 Telegram bot alive to listen command from user
  */
+
+const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379'
+
 const telegramGoLive = async () => {
   const telegramSession = new TelegramSession(true)
   const client = await telegramSession.getTelegramClient()
+  const redis = new Redis(REDIS_URL)
 
   client.addEventHandler(async (event) => {
     // Use the args from event.message.patternMatch.
@@ -26,7 +33,32 @@ const telegramGoLive = async () => {
   }, new NewMessage({}));
 
   const handleSubscribe = async (chatId: bigInt.BigInteger,content: string) => {
+    const splits = content.split(',').map(item => item.trim().toLowerCase())
+    for (const subscribeContent of splits){
+      switch (subscribeContent) {
+        case FUND_NOTIFICATION.FXB_STAG: {
+          const result = await client.invoke(
+            new Api.help.GetUserInfo({
+              userId: chatId,
+            })
+          );
+          await redis.sadd(chatId.toString(), result.toString())
+          break
+        }
+        case FUND_NOTIFICATION.FXB_PROD: {
 
+          break
+        }
+        case FUND_NOTIFICATION.DEX_STAG: {
+
+          break
+        }
+        case FUND_NOTIFICATION.DEX_PROD: {
+
+          break
+        }
+      }
+    }
   }
 
   const handleUnsubscribe = async (chatId: bigInt.BigInteger,content: string) => {
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,11 @@
 const LIMIT_REQUEST_PER_SESSION = 10
+const FUND_NOTIFICATION = {
+  FXB_STAG: 'fxb_stag',
+  FXB_PROD: 'fxb_prod',
+  DEX_STAG: 'dex_stag',
+  DEX_PROD: 'dex_prod',
+}
 export {
   LIMIT_REQUEST_PER_SESSION,
+  FUND_NOTIFICATION
 }
