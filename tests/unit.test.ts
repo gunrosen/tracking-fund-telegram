f65fce3428974aa552b9d3f020193fe5dd0e6c9f
hulk19 <phamhungbkhn@gmail.com>
2023-12-26 15:21:35 +0700

    test: add test

---
+++ tests/unit.test.ts
@@ -0,0 +1,5 @@
+const Constants = require("../src/utils/constants")
+
+test('adds 1 + 2 to equal 3', () => {
+  Object.keys(Constants.FUND_NOTIFICATION).forEach((k,v) => {console.log(k.toString())})
+});
