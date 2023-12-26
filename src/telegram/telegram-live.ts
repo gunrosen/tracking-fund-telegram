85d1aab6869fafa9b4e0e2e1512a3a278e1f979e
Hung Pham <phamhungbkhn@gmail.com>
2023-12-26 23:15:56 +0700

    fix: send loop message

--- src/telegram/telegram-live.ts
+++ src/telegram/telegram-live.ts
@@ -12,161 +12,161 @@ Telegram bot alive to listen command from user
 const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379'
 
 export const telegramGoLive = async () => {
-  const telegramSession = new TelegramSession(true)
-  const client = await telegramSession.getTelegramClient()
-  const redis = new Redis(REDIS_URL)
-  const normalRedis = new Redis(REDIS_URL)
+    const telegramSession = new TelegramSession(true)
+    const client = await telegramSession.getTelegramClient()
+    const redis = new Redis(REDIS_URL)
+    const normalRedis = new Redis(REDIS_URL)
 
-  const me = await client.getMe()
-  let meID
-  if (!(me instanceof Api.InputPeerUser)) {
-    meID = me.id
-  }
-  client.addEventHandler(async (event) => {
-    // Use the args from event.message.patternMatch.
-    const message = event?.message?.message
-    const chatId = event!.message!.chatId
-    console.log(`chatId: ${chatId} meId: ${meID}`)
-    if (message && chatId != meID) {
-      if (message.includes("/subscribe")) {
-        const splits = message.split("subscribe")
-        const content = splits[1].trimStart().trimEnd()
-        await handleSubscribe(chatId, content)
-      } else if (message.includes("/unsubscribe")) {
-        const splits = message.split("unsubscribe")
-        const content = splits[1].trimStart().trimEnd()
-        await handleUnsubscribe(chatId, content)
-      } else if (message.includes("/info")) {
-        await getYourWatchlist(chatId)
-      } else {
-        await wait(1000)
-        console.log('loop')
-        // await client.sendMessage(chatId, {message: "Wrong command, please check"})
-      }
+    const me = await client.getMe(false)
+    let meID
+    if (me instanceof Api.User) {
+        meID = me.id
     }
-  }, new NewMessage({}));
-
-  redis.subscribe(REDIS_PUBSUB_CHANNEL_NAME, (err, count) => {
-    if (err) {
-      console.error("Failed to subscribe: %s", err.message);
-    } else {
-      console.log(
-        `Subscribed successfully! This client is currently subscribed to ${count} channels.`
-      );
-    }
-  })
-
-  redis.on("message", async (channel, message) => {
-    console.log(`Received ${message} from ${channel}`);
-    const {content, env} = buildMessageContent(message)
-    const chatIds = await normalRedis.smembers(`subscribe:${env}`)
-    client.setParseMode("html");
-    for (const chatId of chatIds) {
-      await client.sendMessage( chatId, {message: content,parseMode :'html'})
-    }
-  });
-
-  const handleSubscribe = async (chatId: bigInt.BigInteger, content: string) => {
-    let splits: string[] = content.split(',').map(item => item.trim().toLowerCase())
-    if (splits.includes(FUND_NOTIFICATION.ALL)) {
-      splits = [
-        FUND_NOTIFICATION.FXB_STAG,
-        FUND_NOTIFICATION.FXB_PROD,
-        FUND_NOTIFICATION.DEX_STAG,
-        FUND_NOTIFICATION.DEX_PROD
-      ]
-    }
-    let successList: string[] = []
-    for (const subscribeContent of splits) {
-      switch (subscribeContent) {
-        case FUND_NOTIFICATION.FXB_STAG:
-        case FUND_NOTIFICATION.FXB_PROD:
-        case FUND_NOTIFICATION.DEX_STAG:
-        case FUND_NOTIFICATION.DEX_PROD: {
-          console.log(`${chatId.toString()}-:-${subscribeContent}`)
-          await normalRedis.sadd(`subscribe:${subscribeContent}`, chatId.toString())
-          await normalRedis.sadd(`user:${chatId.toString()}`, subscribeContent)
-          successList.push(subscribeContent)
-          break
+    client.addEventHandler(async (event) => {
+        // Use the args from event.message.patternMatch.
+        const message = event?.message?.message
+        const chatId = event!.message!.chatId
+        const fromId = (event!.message!.fromId as Api.PeerUser)?.userId
+        // console.log(`chatId: ${chatId} meId: ${meID}`)
+        if (message && fromId && fromId.toString() !== meID.toString()) {
+            if (message.includes("/subscribe")) {
+                const splits = message.split("subscribe")
+                const content = splits[1].trimStart().trimEnd()
+                await handleSubscribe(chatId, content)
+            } else if (message.includes("/unsubscribe")) {
+                const splits = message.split("unsubscribe")
+                const content = splits[1].trimStart().trimEnd()
+                await handleUnsubscribe(chatId, content)
+            } else if (message.includes("/info")) {
+                await getYourWatchlist(chatId)
+            } else {
+                await wait(1000)
+                await client.sendMessage(chatId, {message: "Wrong command, please check"})
+            }
         }
-        default: {
-          console.log(`Unsupported: ${subscribeContent}`)
-          break
-        }
-      }
-    }
-    if (successList.length > 0) {
-      await client.sendMessage(chatId, {message: `Subscribed ${successList.join(",")}`})
-    } else {
-      await client.sendMessage(chatId, {message: `Please ensure subscription content valid`})
-    }
-  }
+    }, new NewMessage({}));
 
-  const handleUnsubscribe = async (chatId: bigInt.BigInteger, content: string) => {
-    let splits: string[] = content.split(',').map(item => item.trim().toLowerCase())
-    if (splits.includes(FUND_NOTIFICATION.ALL)) {
-      splits = [
-        FUND_NOTIFICATION.FXB_STAG,
-        FUND_NOTIFICATION.FXB_PROD,
-        FUND_NOTIFICATION.DEX_STAG,
-        FUND_NOTIFICATION.DEX_PROD
-      ]
-    }
-    let successUnList: string[] = []
-    for (const subscribeContent of splits) {
-      switch (subscribeContent) {
-        case FUND_NOTIFICATION.FXB_STAG:
-        case FUND_NOTIFICATION.FXB_PROD:
-        case FUND_NOTIFICATION.DEX_STAG:
-        case FUND_NOTIFICATION.DEX_PROD: {
-          console.log(`${chatId.toString()}-:-${subscribeContent}`)
-          await normalRedis.srem(`subscribe:${subscribeContent}`, chatId.toString())
-          await normalRedis.srem(`user:${chatId.toString()}`, subscribeContent)
-          successUnList.push(subscribeContent)
-          break
+    redis.subscribe(REDIS_PUBSUB_CHANNEL_NAME, (err, count) => {
+        if (err) {
+            console.error("Failed to subscribe: %s", err.message);
+        } else {
+            console.log(
+                `Subscribed successfully! This client is currently subscribed to ${count} channels.`
+            );
         }
-        default: {
-          console.log(`Unsupported: ${subscribeContent}`)
-          break
+    })
+
+    redis.on("message", async (channel, message) => {
+        console.log(`Received ${message} from ${channel}`);
+        const {content, env} = buildMessageContent(message)
+        const chatIds = await normalRedis.smembers(`subscribe:${env}`)
+        client.setParseMode("html");
+        for (const chatId of chatIds) {
+            await client.sendMessage(chatId, {message: content, parseMode: 'html'})
         }
-      }
-    }
-    if (successUnList.length > 0) {
-      await client.sendMessage(chatId, {message: `Unsubscribed ${successUnList.join(",")}`})
-    } else {
-      await client.sendMessage(chatId, {message: `Please ensure unsubscription content valid`})
-    }
-  }
+    });
 
-  const getYourWatchlist = async (chatId: bigInt.BigInteger) => {
-    const watchList = await normalRedis.smembers(`user:${chatId.toString()}`)
-    if (watchList.length > 0) {
-      await client.sendMessage(chatId, {message: `You registered ${watchList.join(",")}`})
-    } else {
-      await client.sendMessage(chatId, {message: `You do not registered`})
+    const handleSubscribe = async (chatId: bigInt.BigInteger, content: string) => {
+        let splits: string[] = content.split(',').map(item => item.trim().toLowerCase())
+        if (splits.includes(FUND_NOTIFICATION.ALL)) {
+            splits = [
+                FUND_NOTIFICATION.FXB_STAG,
+                FUND_NOTIFICATION.FXB_PROD,
+                FUND_NOTIFICATION.DEX_STAG,
+                FUND_NOTIFICATION.DEX_PROD
+            ]
+        }
+        let successList: string[] = []
+        for (const subscribeContent of splits) {
+            switch (subscribeContent) {
+                case FUND_NOTIFICATION.FXB_STAG:
+                case FUND_NOTIFICATION.FXB_PROD:
+                case FUND_NOTIFICATION.DEX_STAG:
+                case FUND_NOTIFICATION.DEX_PROD: {
+                    console.log(`${chatId.toString()}-:-${subscribeContent}`)
+                    await normalRedis.sadd(`subscribe:${subscribeContent}`, chatId.toString())
+                    await normalRedis.sadd(`user:${chatId.toString()}`, subscribeContent)
+                    successList.push(subscribeContent)
+                    break
+                }
+                default: {
+                    console.log(`Unsupported: ${subscribeContent}`)
+                    break
+                }
+            }
+        }
+        if (successList.length > 0) {
+            await client.sendMessage(chatId, {message: `Subscribed ${successList.join(",")}`})
+        } else {
+            await client.sendMessage(chatId, {message: `Please ensure subscription content valid`})
+        }
     }
-  }
 
-  const buildMessageContent = (message: string): { env: string, content: string } => {
-    try {
-      const object = JSON.parse(message)
-      const env = object?.env
-      let content: string = `Env: <b>${env}</b>` + "\n"
-      const details = object?.batch
-      for (const detail of details) {
-        content += detail + "\n"
-      }
-      return {
-        env,
-        content
-      }
-    } catch (e) {
-      return {
-        env: '',
-        content: ''
-      }
+    const handleUnsubscribe = async (chatId: bigInt.BigInteger, content: string) => {
+        let splits: string[] = content.split(',').map(item => item.trim().toLowerCase())
+        if (splits.includes(FUND_NOTIFICATION.ALL)) {
+            splits = [
+                FUND_NOTIFICATION.FXB_STAG,
+                FUND_NOTIFICATION.FXB_PROD,
+                FUND_NOTIFICATION.DEX_STAG,
+                FUND_NOTIFICATION.DEX_PROD
+            ]
+        }
+        let successUnList: string[] = []
+        for (const subscribeContent of splits) {
+            switch (subscribeContent) {
+                case FUND_NOTIFICATION.FXB_STAG:
+                case FUND_NOTIFICATION.FXB_PROD:
+                case FUND_NOTIFICATION.DEX_STAG:
+                case FUND_NOTIFICATION.DEX_PROD: {
+                    console.log(`${chatId.toString()}-:-${subscribeContent}`)
+                    await normalRedis.srem(`subscribe:${subscribeContent}`, chatId.toString())
+                    await normalRedis.srem(`user:${chatId.toString()}`, subscribeContent)
+                    successUnList.push(subscribeContent)
+                    break
+                }
+                default: {
+                    console.log(`Unsupported: ${subscribeContent}`)
+                    break
+                }
+            }
+        }
+        if (successUnList.length > 0) {
+            await client.sendMessage(chatId, {message: `Unsubscribed ${successUnList.join(",")}`})
+        } else {
+            await client.sendMessage(chatId, {message: `Please ensure unsubscription content valid`})
+        }
     }
-  }
+
+    const getYourWatchlist = async (chatId: bigInt.BigInteger) => {
+        const watchList = await normalRedis.smembers(`user:${chatId.toString()}`)
+        if (watchList.length > 0) {
+            await client.sendMessage(chatId, {message: `You registered ${watchList.join(",")}`})
+        } else {
+            await client.sendMessage(chatId, {message: `You do not registered`})
+        }
+    }
+
+    const buildMessageContent = (message: string): { env: string, content: string } => {
+        try {
+            const object = JSON.parse(message)
+            const env = object?.env
+            let content: string = `Env: <b>${env}</b>` + "\n"
+            const details = object?.batch
+            for (const detail of details) {
+                content += detail + "\n"
+            }
+            return {
+                env,
+                content
+            }
+        } catch (e) {
+            return {
+                env: '',
+                content: ''
+            }
+        }
+    }
 }
 
 
