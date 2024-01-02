64509783a0e80fb59018e05d7e78773311f51159
hulk19 <phamhungbkhn@gmail.com>
2024-01-02 11:31:02 +0700

    fix: relayer bsc dex_stag

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,6 +9,9 @@ const FUND_NOTIFICATION = {
   ALL: 'all',
 }
 const REDIS_PUBSUB_CHANNEL_NAME = 'alert-balance'
+const DEFAULT_MIN_NATIVE_MAINNET= 0.5
+const DEFAULT_MIN_NATIVE_TESTNET= 0.3
+const DEFAULT_MIN_LINK = 5
 
 const ENVIRONMENT = {
   "fxb_stag": [
@@ -17,14 +20,14 @@ const ENVIRONMENT = {
       chain: Web3SupportNetwork.BSC_TESTNET,
       type: TrackingType.NATIVE,
       destination: '0xC84F9b16e48649732674F2Af237335CCFB91517f',
-      min: 0.5
+      min: DEFAULT_MIN_NATIVE_TESTNET
     },
     {
       text: "LINK fund",
       chain: Web3SupportNetwork.BSC_TESTNET,
       type: TrackingType.LINK,
       destination: '3130',
-      min: 5
+      min: DEFAULT_MIN_LINK
     }
   ]
   ,
@@ -34,14 +37,14 @@ const ENVIRONMENT = {
       chain: Web3SupportNetwork.BSC_MAINNET,
       type: TrackingType.NATIVE,
       destination: '0x96D7D0CB2af031820Bc6D563ecA14FE4044f1C50',
-      min: 0.3
+      min: DEFAULT_MIN_NATIVE_MAINNET
     },
     {
       text: "LINK fund",
       chain: Web3SupportNetwork.BSC_MAINNET,
       type: TrackingType.LINK,
       destination: '962',
-      min: 5
+      min: DEFAULT_MIN_LINK
     }
   ],
   "dex_stag":[
@@ -49,57 +52,57 @@ const ENVIRONMENT = {
       text: "Relayer",
       chain: Web3SupportNetwork.BSC_TESTNET,
       type: TrackingType.NATIVE,
-      destination: '0xe921F0bC81Be4aB874eE36Aa381059642966B04a',
-      min: 0.3
+      destination: '0x7427A6c7285B765c40b209724Bc4238E42ED3525',
+      min: DEFAULT_MIN_NATIVE_TESTNET
     },
     {
       text: "LINK fund",
       chain: Web3SupportNetwork.BSC_TESTNET,
       type: TrackingType.LINK,
       destination: '3238',
-      min: 5
+      min: DEFAULT_MIN_LINK
     },
     {
       text: "Relayer",
       chain: Web3SupportNetwork.AVALANCHE_FUJI,
       type: TrackingType.NATIVE,
       destination: '0x92bbC4283f47eC3d8fAb4DD733fd170Cb405984e',
-      min: 0.3
+      min: DEFAULT_MIN_NATIVE_TESTNET
     },
     {
       text: "LINK fund",
       chain: Web3SupportNetwork.AVALANCHE_FUJI,
       type: TrackingType.LINK,
       destination: '798',
-      min: 5
+      min: DEFAULT_MIN_LINK
     },
     {
       text: "Relayer",
       chain: Web3SupportNetwork.POLYGON_MUMBAI,
       type: TrackingType.NATIVE,
       destination: '0xEd20674bD2d4E759f830219a1638a934F225c7Af',
-      min: 0.3
+      min: DEFAULT_MIN_NATIVE_TESTNET
     },
     {
       text: "LINK fund",
       chain: Web3SupportNetwork.POLYGON_MUMBAI,
       type: TrackingType.LINK,
       destination: '6588',
-      min: 5
+      min: DEFAULT_MIN_LINK
     },
     {
       text: "Relayer",
       chain: Web3SupportNetwork.ARBITRUM_SEPOLIA,
       type: TrackingType.NATIVE,
       destination: '0x3207D99E05eCa94B17B7165C3f0bA0D0E0761ed6',
-      min: 0.3
+      min: DEFAULT_MIN_NATIVE_TESTNET
     },
     {
       text: "LINK fund",
       chain: Web3SupportNetwork.ARBITRUM_SEPOLIA,
       type: TrackingType.LINK,
       destination: '42',
-      min: 5
+      min: DEFAULT_MIN_LINK
     }
   ]
   ,
